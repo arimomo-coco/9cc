971928d9c98e3805ceed2864384a4215af6f79d1
arintu <coyn0123@gmail.com>
2021-01-07 21:24:49 +0900

    エラーメッセージを改良

--- 9cc.c
+++ 9cc.c
@@ -27,6 +27,25 @@ struct Token
 /* 現在着目しているトークン */
 Token *token;
 
+/* 入力プログラム */
+char* user_input;
+
+/* エラー箇所を報告する */
+void error_at(char* loc, char* fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  // fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%s\n", loc);
+  //fprintf(stderr, "%*s", pos, " "); /* pos個の空白を出力 */
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
 /* エラーを報告するための関数 */
 /* printfと同じ引数を取る */
 void error(char *fmt, ...)
@@ -62,7 +81,7 @@ void expect(char op)
 int expect_number()
 {
   if (token->kind != TK_NUM)
-    error("数ではありません");
+    error_at(token->str, "数ではありません");
   int val = token->val;
   token = token->next;
   return val;
